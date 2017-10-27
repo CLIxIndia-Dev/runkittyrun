2741bb6fb800ce845b2ee4266c5906157b73c6f1
Brandon <bhanks@mit.edu>
2017-10-27 14:35:55 -0400

    fixed timing and related chart bugs

--- en/src/loading.js
+++ en/src/loading.js
@@ -1,5 +1,5 @@
 loading = {
-    preload: function() {
+    preload: function () {
         this.game.load.spritesheet('cat', 'assets/cat.png', 80, 80);
         this.game.load.spritesheet('mouse', 'assets/mouse.png', 80, 80);
         this.game.load.image('box', 'assets/box.png');
@@ -12,10 +12,10 @@ loading = {
         this.game.load.image('clixlogo', 'assets/clixlogo.png');
         this.game.load.image('logo-load', 'assets/logo-load.png');
         this.game.load.image('loadscreen', 'assets/loadscreen.png');
-        
+
         this.game.load.image('dropdown_button', 'assets/dropdown_button_bnw.png');
         this.game.load.image('menu_button', 'assets/menu_button_bnw.png');
-                
+
         this.game.load.image('speed-box', 'assets/speed-box.png');
         this.game.load.image('speed-button', 'assets/speed-button.png');
         this.game.load.image('speed-displaybox', 'assets/speed-displaybox.png');
@@ -23,7 +23,7 @@ loading = {
         this.game.load.image('speed-slider', 'assets/speed-slider.png');
 
         this.game.load.spritesheet('betButtons', 'assets/betButtons.png', 97, 36);
-        
+
         this.game.load.image('footer', 'assets/footer.png');
 
         this.game.load.image('delay-box', 'assets/delay-box.png');
@@ -35,7 +35,7 @@ loading = {
         this.game.load.image('sideplate-p2', 'assets/sideplate-p2.png');
 
         this.game.load.image('legend', 'assets/legend.png');
-        
+
         this.game.load.image('background', 'assets/background.png');
 
 
@@ -47,39 +47,39 @@ loading = {
 
         this.game.load.image('score-plate', 'assets/score-plate.png');
         this.game.load.image('star', 'assets/star.png')
-        
-        this.game.load.image('ending', 'assets/ending.png')
-        
 
-        
-        // // http://phaser.io/docs/2.4.4/Phaser.ScaleManager.html#scaleMode  
+        this.game.load.image('ending', 'assets/ending.png')
+
+        // http://phaser.io/docs/2.4.4/Phaser.ScaleManager.html#scaleMode  
         // game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
-        
-//        this.game.scale.setUserScale(.85, .85, 0, 0)
-//        this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+
+        // this.game.scale.setUserScale(.85, .85, 0, 0)
+        // this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     },
-    create: function() {
-        this.game.state.onStateChange.add(function (state) { console.log(state);}); 
-    
+    create: function () {
+        this.game.state.onStateChange.add(function (state) {
+            console.log(state);
+        });
+
         this.game.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'background');
 
-        this.logo =  this.game.add.sprite(this.game.width/2, 300, 'logo-full');
+        this.logo = this.game.add.sprite(this.game.width / 2, 300, 'logo-full');
         this.logo.anchor.setTo(0.5);
-        this.clix = this.game.add.sprite(this.game.width/2, 425, 'clixlogo');
+        this.clix = this.game.add.sprite(this.game.width / 2, 425, 'clixlogo');
         this.clix.anchor.setTo(0.5);
         this.clix.scale.setTo(0.5);
-        this.copyright = this.game.add.text(this.game.width/2, this.clix.bottom + 40, '©2017 MIT unless otherwise specified.\nEmail contact@clix.tiss.edu for more information',{
+        this.copyright = this.game.add.text(this.game.width / 2, this.clix.bottom + 40, '©2017 MIT unless otherwise specified.\nEmail contact@clix.tiss.edu for more information', {
             font: '15px Arial',
             fill: 'black',
             fontWeight: 'bold',
             align: "center",
         });
         this.copyright.anchor.set(0.5);
-        
+
         var _this = this;
-        setTimeout(function() {
-                _this.game.state.start("PlayLevel");
-            }, 4000);
-        
+        setTimeout(function () {
+            _this.game.state.start("PlayLevel");
+        }, 4000);
+
     }
 };
